d04241cb7a198e5b150125e5dd14fcce4b6c73f5
Alexander Schier <allo@laxu.de>
2020-03-09 18:24:05 +0100

    Upgrade to Django 3

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from .forms import *
 from django import forms
 from django.http import HttpResponse

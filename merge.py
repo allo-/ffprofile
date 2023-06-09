fea910051b994da2d5f54ca2215a3e1743c25df5
Jan Tatje <jan@jnt.io>
2023-06-09 02:43:04 +0200

    Change merge function to keep input parameters unmodified
    If the second argument is modified, then keys from one setting
    are added to the dict of another setting. This leads to extensions
    the user has not selected being added to policies.json.
    If an element that is not a dictionary or list exists in both `a`
    and `b`, the value from a is used.
    It does not do a full deep copy, copying only dicts that exist in
    both `a` and `b`.

--- merge.py
+++ merge.py
@@ -1,14 +1,12 @@
+# recursively merge a and b, if element exists in both the value from a is used.
 def merge(a, b):
     if isinstance(a, list) and isinstance(b, list):
         return a + b
     elif isinstance(a, dict) and isinstance(b, dict):
-        for key in a:
-            if not key in b:
-                b[key] = a[key]
-            else:
-                b[key] = merge(a[key], b[key])
-        return b
-    else:
-        return a
-
+        c = b|a
+        for key in c:
+            if key in a and key in b:
+                c[key] = merge(a[key],b[key])
+        return c
+    return a
 
